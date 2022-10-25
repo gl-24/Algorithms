da5108497af49eb42846c2f014ed8dee62a81f70
gl-24 <gl.ece43@gmail.com>
2022-10-25 14:45:33 +0530

    :tada:

---
+++ recursion/Print one to n.py
@@ -0,0 +1,13 @@
+def print_one_to_n(n):
+    if n == 1: 
+        print(1) 
+        return 
+    print_one_to_n(n - 1)
+    print(n) 
+
+def main():
+    print_one_to_n(10)
+
+if __name__ == "__main__":
+    main()
+    
