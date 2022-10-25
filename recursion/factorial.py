65f9b4740b63b3c8acd96ffa72ffff85afdeb1af
gl-24 <gl.ece43@gmail.com>
2022-10-25 15:02:33 +0530

    Factorial Using IBH :tada:

---
+++ recursion/factorial.py
@@ -0,0 +1,12 @@
+def factorial(n):
+    if n == 1 or n == 0:
+        return 1
+    
+    return factorial(n-1) * n 
+
+def main():
+    fact = factorial(5)
+    print(fact)
+
+if __name__ == "__main__":
+    main()
