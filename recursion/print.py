11809df0559e818ae2e0664c0c6c7bfdc92699c6
gl-24 <gl.ece43@gmail.com>
2022-10-25 14:54:25 +0530

    IBH :tada:

--- recursion/print.py
+++ recursion/print.py
@@ -8,8 +8,19 @@ def print_one_to_n(n):
     print_one_to_n(n - 1)
     print(n) 
 
+def print_n_to_one(n):
+    print(n)
+    
+    if n == 1:
+        return 
+    
+    print_n_to_one(n-1)
+
 def main():
+    print("One to N")
     print_one_to_n(10)
+    print("N to One")
+    print_n_to_one(5)
 
 if __name__ == "__main__":
     main()
